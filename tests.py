df082c78e3d7ff71c3b2cd620152cd9f32f692db
Andrew Li <andrqwli@live.unc.edu>
2022-08-02 10:59:42 -0400

    added string check and duplicate check

--- tests.py
+++ tests.py
@@ -3,7 +3,19 @@ def isTypeString(dateArray):
     out = []
     for idx, date in enumerate(dateArray):
         if not isinstance(date, str):
-            out.append((i, date))
+            out.append((idx, date))
     
     return out if len(out) > 0 else True
+
+def hasNoDuplicates(dateArray):
+    dateSet = set(dateArray)
+    if len(dateSet) == len(dateArray):
+        return True
+    else:
+        out = []
+        for date in dateSet:
+            if dateArray.count(date) > 1:
+                out.append((date, dateArray.count(date)))
+        return out
+
 
